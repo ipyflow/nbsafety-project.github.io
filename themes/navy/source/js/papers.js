fdca91f6dc73e82c9cf9a97f02fc3cd9f53f84d4
Stephen Macke <stephen.macke@gmail.com>
2020-10-18 19:51:56 -0700

    fix paper link

--- themes/navy/source/js/papers.js
+++ themes/navy/source/js/papers.js
@@ -1,7 +1,7 @@
 var entities = {
     "nbsafety_full": {
         "title": "Fine-Grained Lineage for Safer Notebook Interactions",
-        "link": "papers/nbsafety.pdf",
+        "link": "https://smacke.net/papers/nbsafety.pdf",
         "authors": "Stephen Macke, Hongpu Gong, Doris Jung-Lin Lee, Andrew Head, Doris Xin, Aditya Parameswaran",
         "venue": "Under revision for VLDB 2021",
         "preprint": true,
